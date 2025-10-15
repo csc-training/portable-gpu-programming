d4dc6784b4dd108b5f36316e5f01a8db18380944
Jussi Enkovaara <jussi.enkovaara@csc.fi>
2025-10-15 14:18:25 +0300

    MPI message exchance exercise

---
+++ intro-day/mpi-message-exchange/README.md
@@ -0,0 +1,22 @@
+## Message exchange
+
+In this exercise we practice message (data) passing between MPI processes.
+The provided [exchange.cpp](exchange.cpp) code sends `msgsize` integers from 
+one MPI task to another (from rank 0 to 1) and vice versa. Investigate the code
+and try to understand how it is working.
+
+1. Compile the code (with `CC` or `mpicxx`) and run it with two MPI tasks. Investigate
+   the output.
+
+2. Modify the code so that `msgsize` is the same as `arraysize`, compile, and try to run 
+   it again. You should notice that program deadlocks, so after a short while 
+   kill the Slurm job with `scancel <jobid>`.
+
+   Reason for the deadlock is the both MPI tasks try to send but none of them is receiving
+   (with small messages most MPI implementations use some internal buffering, which allows
+    `MPI_Send` to complete even though receive has not posted yet.).
+
+   Modify the code so that in one of the MPI tasks the order of `MPI_Send` and `MPI_Recv` is switched,
+    *i.e.* one task starts by sending and another one with receiving. Compile the code and run it again,
+   you should notice that the program no longer deadlocks.
+
---
+++ intro-day/mpi-message-exchange/exchange.cpp
@@ -0,0 +1,50 @@
+#include <cstdio>
+#include <vector>
+#include <mpi.h>
+
+int main(int argc, char *argv[])
+{
+    constexpr int arraysize = 100000;
+    constexpr int msgsize =   100;
+    std::vector<int> message(arraysize);
+    std::vector<int> receiveBuffer(arraysize);
+
+    int rank, ntasks;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &ntasks);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (ntasks < 2)
+    {
+        printf("Please run with at least 2 MPI processes\n");
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
+
+    // Initialize message and receive buffer
+    for (int i = 0; i < arraysize; i++)
+    {
+        message[i] = rank;
+        receiveBuffer[i] = -1;
+    }
+
+    int tag = 0;
+
+    if (rank == 0)
+    {
+        // Send total of 'msgsize' integers to rank 1.
+        MPI_Send(message.data(), msgsize, MPI_INT, 1, tag, MPI_COMM_WORLD);
+        MPI_Recv(receiveBuffer.data(), msgsize, MPI_INT, 1, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        printf("Rank %i received %i elements, first %i\n", rank, msgsize, receiveBuffer[0]);
+    }
+    else if (rank == 1)
+    {
+        MPI_Send(message.data(), msgsize, MPI_INT, 0, tag, MPI_COMM_WORLD);
+        MPI_Recv(receiveBuffer.data(), msgsize, MPI_INT, 0, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        printf("Rank %i received %i elements, first %i\n", rank, msgsize, receiveBuffer[0]);
+    }
+
+    MPI_Finalize();
+    return 0;
+}

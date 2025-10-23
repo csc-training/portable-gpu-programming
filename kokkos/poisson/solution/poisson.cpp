e621b570b605d290fee79774686a60926de9310c
Jussi Enkovaara <jussi.enkovaara@csc.fi>
2025-10-23 07:05:55 +0300

    Timing the run time

--- kokkos/poisson/solution/poisson.cpp
+++ kokkos/poisson/solution/poisson.cpp
@@ -10,41 +10,28 @@
 template <typename T>
 void init(T x)
 {
-    int nx = x.extent(0);
-    int ny = x.extent(1);
-    double cx = nx / 2.0;
-    double cy = ny / 2.0;
-    double sigma2 = 0.05 * nx*ny;  // Width of the Gaussian
-    double kx = 20.0 / nx;  // Spatial frequency in x
-    double ky = 10.0 / ny;  // Spatial frequency in y
+  int nx = x.extent(0);
+  int ny = x.extent(1);
+  double cx = nx / 2.0;
+  double cy = ny / 2.0;
+  double sigma2 = 0.05 * nx*ny;  // Width of the Gaussian
+  double kx = 20.0 / nx;  // Spatial frequency in x
+  double ky = 10.0 / ny;  // Spatial frequency in y
 
-    Kokkos::parallel_for("init",
-      Kokkos::MDRangePolicy<Kokkos::Rank<2> >({0, 0}, {nx, ny}),
-        KOKKOS_LAMBDA(const int i, const int j) {
-          double dx = j - cx;
-          double dy = i - cy;
-          double r2 = dx * dx + dy * dy;
-          x(i, j) = cos(kx * dx + ky * dy) * exp(-r2 / sigma2);
-        });
+  Kokkos::parallel_for("init",
+    Kokkos::MDRangePolicy<Kokkos::Rank<2> >({0, 0}, {nx, ny}),
+      KOKKOS_LAMBDA(const int i, const int j) {
+        double dx = j - cx;
+        double dy = i - cy;
+        double r2 = dx * dx + dy * dy;
+        x(i, j) = cos(kx * dx + ky * dy) * exp(-r2 / sigma2);
+      });
 }
  
-int main(int argc, char** argv)
+void run(const int n, const int niter)
 {
-  Kokkos::initialize();
-  {
-  Kokkos::Timer timer;
 
-  constexpr int niters = 500;
-
-  int nx, ny;
-  if (2 == argc) {
-    nx = atoi(argv[1]);
-    ny = atoi(argv[1]);
-  } else {
-     nx = NX;
-     ny = NY;
-  }
-
+  const int nx = n, ny = n;
   Kokkos::View<double**> f("f", nx, ny);
   Kokkos::View<double**> u("u", nx, ny);
   Kokkos::View<double**> unew("unew", nx, ny);
@@ -54,9 +41,12 @@ int main(int argc, char** argv)
   Kokkos::deep_copy(unew, 0.0);
   init(f);
 
+  Kokkos::Timer timer;
+  double t0 = timer.seconds();
+
   // Jacobi iteration
   #pragma nounroll
-  for (int iter = 0; iter < niters; iter++) {
+  for (int iter = 0; iter < niter; iter++) {
     Kokkos::parallel_for("jacobi",
       Kokkos::MDRangePolicy<Kokkos::Rank<2> >({1, 1}, {nx-1, ny-1}),
         KOKKOS_LAMBDA(const int i, const int j) {
@@ -88,13 +78,43 @@ int main(int argc, char** argv)
 
   mean /= ((nx - 1) * (ny - 1));
 
+  double t1 = timer.seconds();
+  double elapsed_seconds = t1 - t0;
+
   int i = ny / 2, j = nx / 2;
-  std::cout << "Center u " << unew_host(i, j) << std::endl;
-  std::cout << "Mean u " << mean << std::endl;
+  printf("u[%d,%d] = %f\n", i, j, unew_host(i, j));
+  printf("Mean u = %f\n", mean);
+  printf("Time spent: %6.3f s\n", elapsed_seconds);
 
+}
+
+int main(int argc, char *argv[])
+{
+  Kokkos::initialize();
+  // Array size
+  int n = 1024;
+
+  // Number of iterations
+  int niter = 500;
+
+  if (argc > 2) {
+      niter = std::atoi(argv[2]);
+      if (niter < 1) {
+          printf("Number of iterations need to be greater than zero.\n");
+          return 1;
+      }
   }
-  Kokkos::finalize();
+  if (argc > 1) {
+      n = std::atoi(argv[1]);
+      if (n < 1) {
+          printf("Size needs to be greater than zero.\n");
+          return 1;
+      }
+  }
 
+  run(n, niter);
+
+  Kokkos::finalize();
   return 0;
 }
 

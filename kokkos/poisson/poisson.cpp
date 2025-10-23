7efcc749f3c11848c9f6f9ea2e791af8cd846eaa
Jussi Enkovaara <jussi.enkovaara@csc.fi>
2025-10-23 07:05:55 +0300

    Timing the run time

--- kokkos/poisson/poisson.cpp
+++ kokkos/poisson/poisson.cpp
@@ -2,13 +2,7 @@
 #include <vector>
 #include <cstdio>
 #include <cmath>
-
-#define NX 1024
-#define NY 1024
-
-// C macros shouldn't really be used, but we want to use
-// here 2D indexing to make porting to Kokkos more straightforward 
-//#define IDX(i, j)  ((i) * (NX) + (j))
+#include <chrono>
 
 // Functor for getting linear index from two dimensional
 struct Idx {
@@ -21,48 +15,44 @@ struct Idx {
 template <typename T>
 void init(T &x, int nx, int ny)
 {
-    double cx = nx / 2.0;
-    double cy = ny / 2.0;
-    double sigma2 = 0.05 * nx*ny;  // Width of the Gaussian
-    double kx = 20.0 / nx;  // Spatial frequency in x
-    double ky = 10.0 / ny;  // Spatial frequency in y
+  double cx = nx / 2.0;
+  double cy = ny / 2.0;
+  double sigma2 = 0.05 * nx*ny;  // Width of the Gaussian
+  double kx = 20.0 / nx;  // Spatial frequency in x
+  double ky = 10.0 / ny;  // Spatial frequency in y
 
-    auto idx = Idx(nx);
+  auto idx = Idx(nx);
 
-    for (int i=0; i < nx; i++)
-      for (int j=0; j < ny; j++) 
-        {
-          double dx = j - cx;
-          double dy = i - cy;
-          double r2 = dx * dx + dy * dy;
-          x[idx(i,j)] = cos(kx * dx + ky * dy) * exp(-r2 / sigma2);
-        }
+  for (int i=0; i < nx; i++)
+    for (int j=0; j < ny; j++) 
+      {
+        double dx = j - cx;
+        double dy = i - cy;
+        double r2 = dx * dx + dy * dy;
+        x[idx(i,j)] = cos(kx * dx + ky * dy) * exp(-r2 / sigma2);
+      }
 }
  
-int main(int argc, char** argv)
+void run(const int n, const int niter)
 {
+  printf("Using n = %d, niter = %d\n", n, niter);
 
-  constexpr int niters = 500;
-
-  int nx, ny;
-  if (2 == argc) {
-    nx = atoi(argv[1]);
-    ny = atoi(argv[1]);
-  } else {
-     nx = NX;
-     ny = NY;
-  }
-
-  std::vector<double> f(nx*ny), u(nx*ny, 0.0), unew(nx*ny, 0.0);
+  const int nx = n, ny = n;
+  const int n2 = nx * ny;
+  std::vector<double> f(n2), u(n2, 0.0), unew(n2, 0.0);
 
   double h2 = 1.0;
 
   init(f, nx, ny);
+
   auto idx = Idx(nx);
+
+  using Clock = std::chrono::high_resolution_clock;
+  auto t0 = Clock::now();
 
   // Jacobi iteration
   #pragma nounroll
-  for (int iter = 0; iter < niters; iter++) {
+  for (int iter = 1; iter < niter + 1; iter++) {
     for (int i=1; i < nx-1; i++)
       for (int j=1; j < ny-1; j++) 
         {
@@ -72,13 +62,14 @@ int main(int argc, char** argv)
     std::swap(u, unew);
   }
 
-
   FILE *file = fopen("u.bin", "wb");
   size_t count = nx*ny;
   fwrite(&count, sizeof(size_t), 1, file);
   // Write the data
   size_t written = fwrite(unew.data(), sizeof(double), count, file);
   fclose(file);
+
+  auto t1 = Clock::now();
 
   // Check the result
   double mean = 0.0;
@@ -90,9 +81,41 @@ int main(int argc, char** argv)
 
   mean /= ((nx - 1) * (ny - 1));
 
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0);
+  double elapsed_seconds = 1.0e-3 * duration.count();
+
   int i = ny / 2, j = nx / 2;
-  std::cout << "Center u " << unew[idx(i,j)] << std::endl;
-  std::cout << "Mean u " << mean << std::endl;
+  printf("u[%d,%d] = %f\n", i, j, u[idx(i, j)]);
+  printf("Mean u = %f\n", mean);
+  printf("Time spent: %6.3f s\n", elapsed_seconds);
+
+}
+
+int main(int argc, char *argv[])
+{
+  // Array size
+  int n = 1024;
+
+  // Number of iterations
+  int niter = 500;
+
+  if (argc > 2) {
+      niter = std::atoi(argv[2]);
+      if (niter < 1) {
+          printf("Number of iterations need to be greater than zero.\n");
+          return 1;
+      }
+  }
+  if (argc > 1) {
+      n = std::atoi(argv[1]);
+      if (n < 1) {
+          printf("Size needs to be greater than zero.\n");
+          return 1;
+      }
+  }
+
+  run(n, niter);
 
   return 0;
 }
+
